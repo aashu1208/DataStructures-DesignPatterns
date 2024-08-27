96f3926fa2f8186751d43fca70741d624252ee52
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-27 16:11:17 +0530

    list

---
+++ Data_Structures_List/Data_Structures_List/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Data_Structures_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<int> numbers = new List<int> { 1, 2, 3 };
+            numbers.AddRange(new List<int> { 4, 5, 6 });
+            Console.WriteLine(string.Join(",", numbers));
+        }
+    }
+}
