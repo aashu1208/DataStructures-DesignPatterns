0c4c4c49db5e26477e808cdd85561584c6e90f3a
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-08-28 18:16:40 +0530

    hashset

--- Data_Structures_List/Data_Structures_List/Program.cs
+++ Data_Structures_List/Data_Structures_List/Program.cs
@@ -8,9 +8,36 @@ namespace Data_Structures_List
         {
 
 
-            List<int> numbers = new List<int> { 1, 2, 3 };
-            numbers.AddRange(new List<int> { 4, 5, 6 });
-            Console.WriteLine(string.Join(",", numbers));
+            List<int> numbers = new List<int> {1,2,3,4};
+            //numbers.AddRange(new List<int> { 4, 5, 6 });
+            //numbers.RemoveAt(0);
+            //numbers.Insert(0, 7);
+            //numbers.ToArray();
+            //numbers.Contains(3);
+            //int index = numbers.IndexOf(3);
+            //int even = numbers.Find(x => x % 2 == 0);
+            /*numbers.Sort();
+            numbers.Sort((a, b) => b.CompareTo(a));
+            numbers.Clear();*/
+            //int[] array = numbers.ToArray();
+            //int count = numbers.Count;
+
+            //int cap = numbers.Capacity;
+            //Console.WriteLine(string.Join(",", cap));
+
+            //Console.WriteLine(cap);
+
+            /*string[] stringNumbers = { "1", "2", "3", "4" };
+            Console.WriteLine(string.Join(",", stringNumbers));
+            int[] stringToArray = Array.ConvertAll(stringNumbers, int.Parse);
+            Console.WriteLine(string.Join(",", stringToArray));
+*/
+            HashSet<int> num = new HashSet<int> { 1, 2, 3, 4 };
+            HashSet<int> num2 = new HashSet<int> { 5, 6, 7, 8 };
+
+            num.UnionWith(num2);
+            Console.WriteLine(string.Join(",", num));
+            
         }
     }
 }
