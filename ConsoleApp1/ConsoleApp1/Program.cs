f1aa53cdbd4c4141c7ec4c608e580343dc6ee0ae
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-09-05 23:53:19 +0530

    Target question

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*// Example array
+        int[] num = { 4, -3, 9, -4, -9, 1, 2 };
+
+        // Create a HashSet to store the numbers we've seen
+        HashSet<int> seenNumbers = new HashSet<int>();
+
+        // Iterate through each number in the array
+        foreach (int current in num)
+        {
+            // Check if the opposite number exists in the set
+            if (seenNumbers.Contains(-current))
+            {
+                // If the current number is positive, print it
+                if (current > 0)
+                {
+                    Console.WriteLine("Positive number with its negative counterpart: " + current);
+                }
+                // If the current number is negative, print its positive counterpart
+                else
+                {
+                    Console.WriteLine("Positive number with its negative counterpart: " + -current);
+                }
+            }
+
+            // Add the current number to the set
+            seenNumbers.Add(current);
+        }*/
+        //Find_Pos_Neg_Counterparts_Num();
+        //PairOfNumbers();
+        
+    }
+
+    /*public static void Find_Pos_Neg_Counterparts_Num()
+    {
+        int[] num = { 1, 2, 3, 4, 5, 6, -1, -2, -3 };
+        HashSet<int> numberSeen = new HashSet<int>();
+
+        foreach (int items in num)
+        {
+            if (numberSeen.Contains(-items))
+            
+
+                if(items >0)
+                    Console.WriteLine("positive numbers of negative: "+items);
+            
+
+                else
+                {
+                    Console.WriteLine("Negative number of positive: " + (-items));
+                }
+
+            numberSeen.Add(items);
+        }
+
+        
+
+    }*/
+
+    public static void PairOfNumbers()
+    {
+
+        int[] arr = { 2, 4, 3, 5, 7, 8, 1 };
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for(int j = i+1; j<arr.Length; j++)
+            {
+                if(arr[i]+arr[j] == 6)
+                Console.WriteLine("target achieved using: " + arr[i] + ":" + arr[j]);
+                
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/MissingNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+public class MissingNumber
+{
+	public static void Main(string[] args)
+    {
+        MissingNumber();
+        
+    }
+
+
+    public static void MissingNumber()
+    {
+        int[] num = { 3, 7, 1, 2, 8, 4, 5 };
+        int totalLength = num.Length + 1;
+
+        int arraySum = num.Sum();
+
+        int totalSum = (totalLength * (totalLength + 1)) / 2;
+
+        int missingNumber = totalSum - arraySum;
+        Console.WriteLine("Total Length: " + totalLength);
+        Console.WriteLine("Array Sum: " + arraySum);
+        Console.WriteLine("Total Sum: " + totalSum);
+        Console.WriteLine("Missing Number is: " + missingNumber);
+
+    }
+}
