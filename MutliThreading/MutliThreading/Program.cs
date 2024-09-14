79fb55f4c87375cfda4cb9e256e5fb12782d7bd7
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-09-14 21:49:26 +0530

    multi threading basics

---
+++ MutliThreading/MutliThreading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace MutliThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+
+            Thread addition = new Thread(prog.Add);
+            Thread substraction = new Thread(prog.Sub);
+
+            addition.Start();
+            substraction.Start();
+
+            addition.Join();
+            substraction.Join();
+        }
+
+
+        public void Add()
+        {
+            int result = 5 + 6;
+            Console.WriteLine("Add: " + result);
+
+        }
+
+        public void Sub()
+        {
+            int result = 6 - 5;
+            Console.WriteLine("Sub: " + result);
+
+        }
+    }
+}
