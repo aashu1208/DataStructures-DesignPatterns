6dee6c90c9f79c579d8eb9325bca2184dfffc1b6
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-09-18 23:08:02 +0530

    target numbers

--- MissingNumber/MissingNumber/Program.cs
+++ MissingNumber/MissingNumber/Program.cs
@@ -7,11 +7,12 @@ namespace MissingNumber
     {
         static void Main(string[] args)
         {
-            MissingNumber();
+            //MissingNumber();
 
+            TargetNumber();
         }
 
-        private static void MissingNumber()
+       /* private static void MissingNumber()
         {
             int[] numbers = new int[6] { 1, 2, 3, 5, 6, 7 };
             int lengthOfArray = numbers.Length + 1;
@@ -20,6 +21,25 @@ namespace MissingNumber
 
             int missingNum = sum - sumOfArray;
             Console.WriteLine("Missing Number is: " + missingNum);
+        }*/
+
+        public static void TargetNumber()
+        {
+
+            int[] numbers = new int[6] { 1, 5, 4, 2, 3, 3 };
+
+            int target = 6;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i]+numbers[j] == target)
+                    {
+                        Console.WriteLine(numbers[i] + " + " + numbers[j] + "=" + target);
+                    }
+                }
+            }
         }
     }
 }
