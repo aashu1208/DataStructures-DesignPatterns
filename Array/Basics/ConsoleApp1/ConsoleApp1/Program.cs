f3390eca2858a07d55e6b606583762f321fb1220
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-10-01 00:06:15 +0530

    peak element

--- Array/Basics/ConsoleApp1/ConsoleApp1/Program.cs
+++ Array/Basics/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3 };
+            int n = arr.Length;
+
+            if (n == 0)
+            {
+                Console.WriteLine("no elements in the array");
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if ((i == 0 || arr[i]>=arr[i-1]) && (i == n-1 || arr[i]>= arr[i+1]))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine(-1);
         }
     }
 }
