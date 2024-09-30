606e498882d13dc2325c78c0d37c7e1ed8fcdbad
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-09-30 23:24:50 +0530

    Array leader, peak element

---
+++ Array/ArrayLeaders/ArrayLeaders/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayLeaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 4, 2, 4, 1 };
+            int n = arr.Length;
+            int maxfromRight = arr[n - 1];
+            List<int> result = new List<int>();
+
+
+            result.Add(maxfromRight);
+            for (int i = n-2; i >= 0; i--)
+            {
+                if (arr[i]>=maxfromRight)
+                {
+                    maxfromRight = arr[i];
+                    result.Add(arr[i]);
+                }
+            }
+
+
+            result.Reverse();
+            foreach (int leaders in result)
+            {
+                Console.WriteLine(string.Join(",", leaders));
+            }
+        }
+    }
+
+}
---
+++ Array/Subarray Sum/Subarray Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Subarray_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            int n = 5;
+            int targetsum = 12;
+
+            foreach(int num in arr)
+            {
+
+
+            }*/
+
+        }
+    }
+}
