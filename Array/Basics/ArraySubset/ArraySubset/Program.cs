3b6b5a9c146634d61a8d1c88e966c4ab16d53ab4
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-10-02 22:18:17 +0530

    array subset with 51/209 passing cases

--- Array/Basics/ArraySubset/ArraySubset/Program.cs
+++ Array/Basics/ArraySubset/ArraySubset/Program.cs
@@ -6,21 +6,23 @@ namespace ArraySubset
     {
         static void Main(string[] args)
         {
-            
+            int[] a1 = { 1, 2, 3, 4, 5, 6 };
+            int[] a2 = { 1, 2, 3, 4 };
+            int m = a2.Length;
+            int n = a1.Length;
+            Console.WriteLine (isSubset(a1, a2, m,n));
         }
 
         public static string isSubset(int[] a1, int[] a2, int m, int n)
         {
-
             HashSet<int> set1 = new HashSet<int>(a1);
             for (int i = 0; i < m; i++)
             {
-                if (!set1.Contains(a2[i]))
+                if(!set1.Contains(a2[i]))
                 {
                     return "No";
                 }
             }
-
             return "Yes";
         }
     }
