fc3b1b8ba43479ec195bde90c788583a0ef7b8e8
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-10-02 22:11:34 +0530

    array subset

--- Array/Basics/ArraySubset/ArraySubset/Program.cs
+++ Array/Basics/ArraySubset/ArraySubset/Program.cs
@@ -6,7 +6,22 @@ namespace ArraySubset
     {
         static void Main(string[] args)
         {
-            HashSet<int> set1 = new HashSet<int>();
+            
+        }
+
+        public static string isSubset(int[] a1, int[] a2, int m, int n)
+        {
+
+            HashSet<int> set1 = new HashSet<int>(a1);
+            for (int i = 0; i < m; i++)
+            {
+                if (!set1.Contains(a2[i]))
+                {
+                    return "No";
+                }
+            }
+
+            return "Yes";
         }
     }
 }
