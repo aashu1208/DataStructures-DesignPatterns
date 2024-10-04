a4fe5de0def9fab2e0c483ab9cae086b5a191e3d
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2024-10-04 17:39:33 +0530

    Two sum

---
+++ Array/Basics/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] num = { 1, 3, 5, 4, 6 };
+            int target = 9;
+
+            Console.WriteLine(TwoSum(num, target));
+
+        }
+
+
+        public static int[] TwoSum(int[] num, int target)
+        {
+
+            
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = i+1; j < num.Length; j++)
+                {
+                    if (num[i]+num[j] == target)
+                    {
+                        return new int[] {i,j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
